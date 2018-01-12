e5e898aa3c5e61ea70fda475fa9582d5bec7abd5
Simon C. Wu <simon.c.wu@gmail.com>
2018-01-12 21:35:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,8 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  
+  let ranks = document.querySelectorAll('.ranked-list');
+  for (let i = 0; i < ranks.length; i++) {
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + 1;
+  }
 }
